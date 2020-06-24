3077b913c2b85dc29cbc35d5e0b856a18115325e
Illyoung Choi <iychoi@email.arizona.edu>
2020-06-24 11:01:01 -0700

    Move to python3

--- bin/irodsFsCtl.py
+++ bin/irodsFsCtl.py
@@ -1,7 +1,4 @@
-#! /usr/bin/env python
-
-# This code is written by Illyoung Choi (iychoi@email.arizona.edu)
-# funded by iPlantCollaborative (www.iplantcollaborative.org).
+#! /usr/bin/env python3
 
 import os
 import os.path
@@ -38,50 +35,50 @@ def _IOC(dir, type, nr, size):
 
 def _IO(type, nr):
     return _IOC(_IOC_NONE, type, nr, 0)
-    
+
 def _IOR(type, nr, size):
     return _IOC(_IOC_READ, type, nr, size)
 
 def _IOW(type, nr, size):
     return _IOC(_IOC_WRITE, type, nr, size)
-    
+
 def _IOWR(type, nr, size):
     return _IOC(_IOC_READ | _IOC_WRITE, type, nr, size)
 
 
 
 def reset_cache(mount_path):
-    print "reset cache: %s" % (mount_path)
-    
+    print("reset cache: %s" % (mount_path))
+
     fd = os.open(mount_path, os.O_DIRECTORY)
     status = fcntl.ioctl(fd, _IO(IOCTL_APP_NUMBER, IFUSEIOC_RESET_METADATA_CACHE))
     if status != 0:
-        print >> sys.stderr, "failed to reset cache"
+        print("failed to reset cache", file=sys.stderr)
     else:
-        print "Done!"
+        print("Done!")
     os.close(fd)
-    
+
 def show_connections(mount_path):
-    print "show connections: %s" % (mount_path)
-    
+    print("show connections: %s" % (mount_path))
+
     fd = os.open(mount_path, os.O_DIRECTORY)
     buf = array.array('i', [0,0,0,0,0])
     status = fcntl.ioctl(fd, _IOR(IOCTL_APP_NUMBER, IFUSEIOC_SHOW_CONNECTIONS, 20), buf, 1)
     if status != 0:
-        print >> sys.stderr, "failed to show connections"
+        print("failed to show connections", file=sys.stderr)
     else:
         inuseShortOpConn = buf[0]
         inuseConn = buf[1]
         inuseOnetimeuseConn = buf[2]
         freeShortopConn = buf[3]
         freeConn = buf[4]
-        
-        print "In-Use ShortOp Conn: %d" % inuseShortOpConn
-        print "In-Use Conn: %d" % inuseConn
-        print "In-Use OneTimeUse Conn: %d" % inuseOnetimeuseConn
-        print "Free ShortOp Conn: %d" % freeShortopConn
-        print "Free Conn: %d" % freeConn
-        print "Done!"
+
+        print("In-Use ShortOp Conn: %d" % inuseShortOpConn)
+        print("In-Use Conn: %d" % inuseConn)
+        print("In-Use OneTimeUse Conn: %d" % inuseOnetimeuseConn)
+        print("Free ShortOp Conn: %d" % freeShortopConn)
+        print("Free Conn: %d" % freeConn)
+        print("Done!")
     os.close(fd)
 
 COMMANDS = {
@@ -96,28 +93,28 @@ COMMANDS_DESCS = {
 
 def ioctl(command, mount_path, oargs):
     if command is None:
-        print >> sys.stderr, "No command is given"
-    
+        print("No command is given", file=sys.stderr)
+
     command = command.lower()
     mount_path = os.path.abspath(mount_path)
-    
+
     if command in COMMANDS:
         COMMANDS[command](mount_path)
     else:
-        print >> sys.stderr, "Unrecognized command: %s" % (command)
+        print("Unrecognized command: %s" % (command), file=sys.stderr)
 
 def main(argv):
     if len(argv) < 2:
-        print "command : ./irodsFsCtl.py <COMMAND> <IRODSFS_MOUNT_PATH> <Optional_Args...>"
-        print ""
-        print "Available Commands"
+        print("command : ./irodsFsCtl.py <COMMAND> <IRODSFS_MOUNT_PATH> <Optional_Args...>")
+        print("")
+        print("Available Commands")
         for k in COMMANDS.keys():
-            print "%s: %s" % (k, COMMANDS_DESCS[k])
+            print("%s: %s" % (k, COMMANDS_DESCS[k]))
     else:
         command = argv[0]
         mount_path = argv[1]
         oargs = argv[2:]
-        
+
         ioctl(command, mount_path, oargs)
 
 if __name__ == "__main__":
